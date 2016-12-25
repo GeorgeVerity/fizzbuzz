42ed41cf1cf13fe732b090b891ae19b297724151
GeorgeVerity <georgemverity@gmail.com>
2016-12-25 18:26:50 -0500

    push 2
    nothing new

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,6 +2,7 @@
 for (i=1; i<=20; i++){
   //for each number, do the following check:
 
+  //see if it is divisible by 3
   if (i % 3 === 0) {
     //if so: see if it is also divisible by 5
     if (i % 5 === 0) {
